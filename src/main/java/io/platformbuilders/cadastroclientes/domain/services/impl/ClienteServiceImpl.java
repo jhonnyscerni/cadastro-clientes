03c9e879a25a08123067b00952de106cb0b26b48
jhonnyscerni <jhonnyscerni@gmail.com>
2023-02-05 22:54:46 -0300

    tests

--- src/main/java/io/platformbuilders/cadastroclientes/domain/services/impl/ClienteServiceImpl.java
+++ src/main/java/io/platformbuilders/cadastroclientes/domain/services/impl/ClienteServiceImpl.java
@@ -19,6 +19,8 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
